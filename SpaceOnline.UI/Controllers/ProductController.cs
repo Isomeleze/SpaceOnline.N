361aa22d234706d7bae34c635cf778a0484f578d
Isomeleze Molosi <molosi@tokkie.onmicrosoft.com>
2022-09-21 12:50:20 +0200

    Added Product Contoller

---
+++ SpaceOnline.UI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using SpaceOnline.Core.Models;
+using SpaceOnline.DataAccess.Inmemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpaceOnline.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductRepository context;
+        public ProductController()
+        {
+            context = new ProductRepository();
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View();
+        }
+    }
+}
