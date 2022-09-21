9ca70cb1a1b031bb667ba99572badcb2f670e942
Isomeleze Molosi <molosi@tokkie.onmicrosoft.com>
2022-09-21 18:53:47 +0200

    Added Category views and controller

---
+++ SpaceOnline.UI/Controllers/CategoryController.cs
@@ -0,0 +1,100 @@
+﻿using SpaceOnline.Core.Models;
+using SpaceOnline.DataAccess.Inmemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpaceOnline.UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryRepository context;
+        public CategoryController()
+        {
+            context = new CategoryRepository();
+        }
+        // GET: Category
+        public ActionResult Index()
+        {
+            List<ProductCategory> categories = context.Collection().ToList();
+            return View(categories);
+        }
+        public ActionResult Create()
+        {
+            ProductCategory productCategory = new ProductCategory();
+            return View(productCategory);
+        }
+        [HttpPost]
+        public ActionResult Create(ProductCategory productCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productCategory);
+            }
+            else
+            {
+                context.Insert(productCategory);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            ProductCategory productCategory = context.Find(Id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productCategory);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(ProductCategory productCategory, string Id)
+        {
+            ProductCategory CategoryToEdit = context.Find(Id);
+            if (CategoryToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                CategoryToEdit.Category = productCategory.Category;
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            ProductCategory CategoryToDelete = context.Find(Id);
+            if (CategoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(CategoryToDelete);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            ProductCategory CategoryToDelete = context.Find(Id);
+            if (CategoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
